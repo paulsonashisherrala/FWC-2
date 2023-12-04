9d90dd9ec16ddf896b3189f425af309a99a55470
paulsonashisherrala <paulsonashisherrala24@gmail.com>
2023-12-04 14:10:31 +0530

    Add files via upload

---
+++ Assignments/geometry/Triangle/EigenVAlues_vectors/Lists(DSA)/libs/randfun.h
@@ -0,0 +1,70 @@
+//Function declaration
+void uniform(char *str, int len);
+void gaussian(char *str, int len);
+double mean(char *str);
+//End function declaration
+
+
+//Defining the function for generating uniform random numbers
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
+//End function for generating uniform random numbers
+
+//Defining the function for calculating the mean of random numbers
+double mean(char *str)
+{
+int i=0,c;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+x;
+}
+fclose(fp);
+temp = temp/(i-1);
+return temp;
+
+}
+//End function for calculating the mean of random numbers
+
+//Defining the function for generating Gaussian random numbers
+void gaussian(char *str, int len)
+{
+int i,j;
+double temp;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+temp = 0;
+for (j = 0; j < 12; j++)
+{
+temp += (double)rand()/RAND_MAX;
+}
+temp-=6;
+fprintf(fp,"%lf\n",temp);
+}
+fclose(fp);
+
+}
+//End functon for generating Gaussian random numbersi
